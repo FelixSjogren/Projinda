9823fba9bd91c546f3bb704afcff2356182c507b
John Söderholm <jsoderholm@JS-MBPRO.local>
2022-05-02 15:21:52 +0200

    Börjat med att skapa mark.

--- src/main.go
+++ src/main.go
@@ -1,7 +1,18 @@
 package main
 
 import (
+	"image/color"
+
 	"github.com/hajimehoshi/ebiten/v2"
+)
+
+const (
+	windowHeight = 640
+	windowWidth  = 960
+)
+
+var (
+	background = color.White
 )
 
 func main() {
@@ -10,4 +21,14 @@ func main() {
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		panic(err)
 	}
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return windowWidth, windowHeight
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(background)
+	g.player.drawPL(screen)
+	g.drawBG(bgImage)
 }
